6ff120f7f4cbac4cdfc3a6d8dc02be4c99119090
Nitinpal269 <76909634+Nitinpal269@users.noreply.github.com>
2021-02-07 22:52:07 +0530

    wrapper class program

---
+++ Infinity.java
@@ -0,0 +1,10 @@
+class Infinity
+{
+	public static void main(String[] args)
+	{
+		double b1=2.4/0;
+		System.out.println(b1);
+		Double b2=-2.4/0;
+		System.out.println(b2);
+	}
+}
---
+++ Parsing.java
@@ -0,0 +1,14 @@
+class Parsing
+{
+	public static void main(String[] args)
+	{
+		int i1=Integer.parseInt("1010",2);
+		int i6=Integer.parseInt("10");
+		long i2=Long.parseLong("10");
+		double d1=Double.parseDouble("3.123");
+		System.out.println(i1);
+		System.out.println(i6);
+		System.out.println(i2);
+		System.out.println(i1);
+	}
+}
